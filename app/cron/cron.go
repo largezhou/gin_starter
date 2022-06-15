cda252bbb2c083e38d87fc3ffb5080a6e4d406a3
largezhou <largezhou@hotmail.com>
2022-06-15 16:05:23 +0800

    展示示例定时任务，配置默认不开启定时任务

--- app/cron/cron.go
+++ app/cron/cron.go
@@ -1,14 +1,14 @@
 package cron
 
-// import (
-// 	"github.com/largezhou/gin_starter/app"
-// 	"github.com/largezhou/gin_starter/app/helper"
-// 	"github.com/largezhou/gin_starter/app/logger"
-// )
+import (
+	"github.com/largezhou/gin_starter/app"
+	"github.com/largezhou/gin_starter/app/helper"
+	"github.com/largezhou/gin_starter/app/logger"
+)
 
 func init() {
-	// app.NewCron("*/5 * * * * ?", func() {
-	// 	ctx := helper.NewTraceIdContext()
-	// 	logger.Debug(ctx, "cron")
-	// }).SkipIfStillRunning()
+	app.NewCron("*/5 * * * * ?", func() {
+		ctx := helper.NewTraceIdContext()
+		logger.Debug(ctx, "cron")
+	}).SkipIfStillRunning()
 }
